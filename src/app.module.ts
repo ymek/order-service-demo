92889b287e3a23120c22aaae2d98e845d82e03e2
Myke Ahn-Stubbs <myke@mykestubbs.com>
2025-03-31 01:36:41 -0700

    Update NestJS app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { OrdersModule } from './orders/orders.module'
+import { CustomerModule } from './customer/customer.module'
+import { InventoryModule } from './inventory/inventory.module'
+import { PaymentsModule } from './payments/payments.module'
+import { MessagingModule } from './messaging/messaging.module'
+import { ShippingModule } from './shipping/shipping.module'
+import { ScheduleModule } from '@nestjs/schedule'
 
 @Module({
-  imports: [],
+  imports: [
+    OrdersModule,
+    CustomerModule,
+    InventoryModule,
+    PaymentsModule,
+    MessagingModule,
+    ShippingModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
